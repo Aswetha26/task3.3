90b8b13b4404311956631882b9ac479f13b9112a
Aswetha26 <swethasarojini555@gmail.com>
2023-10-15 01:25:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-
+import React from 'react';
 import './App.css';
-import { SliderPicker } from "react-color";
-import React from "react";
+import ColorPicker from './components/ColorPicker';
 
-export default function App() {
-  const [color, setColor] = React.useState({ hex: "#FFFFFF" });
+function App() {
   return (
-    <div class='bg'>
-    <center>
     <div className="App">
-      <h1>Color Picker</h1>
-      
-      <SliderPicker
-        color={color}
-        onChangeComplete={(color) => setColor(color)}
-      />
-      <br></br><br></br>
-      <button class='button'>Pick a color</button>
+      <ColorPicker />
     </div>
-    </center></div>
   );
 }
 
+export default App;
